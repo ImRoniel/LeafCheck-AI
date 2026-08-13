20f525a273fec7b04f8081f26136a1420dea1958
Roniel C. Carbon <cuaresmaroniel27@gmail.com>
2026-08-13 19:48:48 +0800

    refactor: change the folder structure to monorepo structure and drop the mvc aricthrecute

--- services/supabase.ts
+++ services/supabase.ts
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 
 import "expo-sqlite/localStorage/install";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "https://placeholder-url.supabase.co";
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "placeholder-key";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
